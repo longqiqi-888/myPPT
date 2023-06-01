4f1d9ca4507814fa5320e67c6bf9e3475808f6a0
lql <1153706397@qq.com>
2023-06-01 15:33:03 +0800

    add：下载依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+// import { URL, fileURLToPath } from "node:url";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    // 设置别名
+    alias:{
+      // "@":fileURLToPath(new URL('./src',import.meta.url))
+      '@': resolve(__dirname,'./src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      // 全局样式引入
+      scss: {
+        additionalData: '@import "@/assets/styles/variables.scss";',
+        javascriptEnabled: true
+      }
+    }
+  },
+  
 })
