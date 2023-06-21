9a2e5f1d1cfd6e9e899ec6ec90aa50787603ad25
lql <1153706397@qq.com>
2023-06-21 14:52:48 +0800

    add：导入样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import router from './router'
 import './style.css'
 import App from './App.vue'
 
+import '@/assets/styles/prosemirror.scss'
+import '@/assets/styles/global.scss'
+import '@/assets/styles/antd.scss'
+import '@/assets/styles/font.scss'
+
 const app = createApp(App)
 
 app.use(router)
